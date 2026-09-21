6d3036deff142fc4c6ab27baf1139002b10f8103
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-09-21 18:11:30 +0300

    feat(admin): Fetch and pass posts and audit logs to dashboard

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
+use App\Entity\AuditLog;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -10,8 +13,15 @@ use Symfony\Component\Routing\Attribute\Route;
 class AdminController extends AbstractController
 {
     #[Route('/', name: 'app_admin_dashboard')]
-    public function index(): Response
+    public function index(EntityManagerInterface $em): Response
     {
-        return $this->render('admin/dashboard.html.twig');
+        $posts = $em->getRepository(Post::class)->findBy([], ['created_at' => 'DESC']);
+        $auditLogs = $em->getRepository(AuditLog::class)->findBy([], ['created_at' => 'DESC'], 10);
+
+        return $this->render('admin/dashboard.html.twig', [
+            'posts' => $posts,
+            'audit_logs' => $auditLogs,
+            'current_admin' => $this->getUser(),
+        ]);
     }
 }
