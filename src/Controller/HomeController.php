f1fe75576ee0b8b62ba4a10ced60cb27184f6f62
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-09-21 17:09:20 +0300

    refactor(web): Update HomeController to use Route attribute

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,7 +6,7 @@ use App\Entity\Post;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
