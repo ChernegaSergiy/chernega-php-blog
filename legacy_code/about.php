5c0e7e1f12e21a739163de72a91148bbf5a4d857
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-09-21 17:02:31 +0300

    refactor: Remove legacy_code backup

--- legacy_code/about.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once __DIR__ . '/bootstrap.php';
-
-echo twig()->render('static/about.html.twig', [
-    'page_title' => 'Про цей блог',
-]);
--- legacy_code/contact.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once __DIR__ . '/bootstrap.php';
-
-echo twig()->render('static/contact.html.twig', [
-    'page_title' => 'Зв\'язок',
-]);
